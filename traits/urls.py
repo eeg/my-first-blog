762d6cd3d3009b7e03b5841bb19aca9c0e47d9aa
Emma Goldberg <emma@gila>
2018-12-04 14:01:45 -0600

    move url logic into views

--- traits/urls.py
+++ traits/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from traits.views import TraitCreateView, TraitSuccessView
 
 urlpatterns = [
-    #url(r'^traits/create/$', TraitCreateView.as_view(success_url='.'), name='traits_create'), # return to same page
-    url(r'^traits/create/$', TraitCreateView.as_view(success_url='../success'), name='traits_create'), # go to traits/success/
+    url(r'^traits/create/$', TraitCreateView.as_view(), name='traits_create'),
     url(r'^traits/success/$', TraitSuccessView.as_view(), name='traits_success'),
 ]
--- traits/views.py
+++ traits/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
 from .models import Trait
 
 class TraitCreateView(CreateView):
     model = Trait
     fields = ('genus', 'species', 'breeding_system', 'isi')
+    success_url = reverse_lazy('traits_success')
     template_name = 'traits/create.html'
 
 class TraitSuccessView(TemplateView):
