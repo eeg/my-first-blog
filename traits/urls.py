f3af25ec3019886d1e3d4e71574f9e35f47c666a
Emma Goldberg <emma@gila>
2018-12-04 13:54:31 -0600

    very minor tidying

--- traits/urls.py
+++ traits/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from traits.views import TraitCreateView, TraitSuccessView
 
 urlpatterns = [
-    #url(r'^traits/create/$', TraitCreateView.as_view(success_url='.'), name='create_trait'), # return to same page
-    url(r'^traits/create/$', TraitCreateView.as_view(success_url='../success'), name='create_trait'), # go to traits/success/
+    #url(r'^traits/create/$', TraitCreateView.as_view(success_url='.'), name='traits_create'), # return to same page
+    url(r'^traits/create/$', TraitCreateView.as_view(success_url='../success'), name='traits_create'), # go to traits/success/
     url(r'^traits/success/$', TraitSuccessView.as_view(), name='traits_success'),
 ]
--- traits/views.py
+++ traits/views.py
@@ -4,7 +4,7 @@ from .models import Trait
 class TraitCreateView(CreateView):
     model = Trait
     fields = ('genus', 'species', 'breeding_system', 'isi')
-    template_name = 'traits/create_trait.html'
+    template_name = 'traits/create.html'
 
 class TraitSuccessView(TemplateView):
     template_name = 'traits/success.html'
