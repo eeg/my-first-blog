b2f9cf87fe8cc7c5214edf17b77586eb80268488
Emma Goldberg <emma@gila>
2019-04-10 09:23:26 -0500

    tried out skip_unchanged option in import-export

--- traits/resources.py
+++ traits/resources.py
@@ -17,8 +17,13 @@ class TraitResource(resources.ModelResource):
         model = Trait
         clean_model_instances = True
         #  fields = ('id', 'genus', 'species', 'isi', 'breeding_system', 'pubref')
-        #  report_skipped = True
-        #  skip_unchanged = True
+
+        report_skipped = True
+        skip_unchanged = True
+        # "unchanged" applies to rows that are identical to existing ones, including 
+        # the unique id and all other columns present in the imported file
+        #     skip_changed = False: identical imported rows are recorded as updates
+        #     skip_changed = True:  identical imported rows are skipped
 
     def before_import(self, dataset, using_transactions, dry_run=True, collect_failed_rows=False, **kwargs):
         if 'id' not in dataset.headers:
