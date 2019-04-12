381a963260665d09699a7324304e483b761efed6
Emma Goldberg <emma@gila>
2019-04-12 15:08:57 -0500

    found how to get all the fields of a model
    [field.name for field in Trait._meta.get_fields()]

--- traits/admin.py
+++ traits/admin.py
@@ -10,7 +10,8 @@ class TraitInline(admin.TabularInline):
 
 class TraitAdmin(ImportExportModelAdmin, SimpleHistoryAdmin):
     list_display = ('__str__', 'pubref')
-    history_list_display = ['breeding_system', 'isi', 'pubref']
+    #  history_list_display = ['breeding_system', 'isi', 'pubref']
+    history_list_display = [field.name for field in Trait._meta.get_fields()]
     resource_class = TraitResource
 
 admin.site.register(Trait, TraitAdmin)
